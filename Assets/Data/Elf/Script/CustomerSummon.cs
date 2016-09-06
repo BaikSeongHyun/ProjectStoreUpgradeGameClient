e18935afdb87c7464764b9adab4ddf2186986698
unknown <dds4120@naver.com>
2016-09-06 17:17:15 +0900

    2016.09.06 오후 돗자리에 물건 올리기 가능

---
+++ Assets/Data/Elf/Script/CustomerSummon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomerSummon : MonoBehaviour 
+{
+
+	public GameObject Customer;
+	public float interval;
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine ( CustomerSummonCoroutine() );	
+	}
+	
+	// Update is called once per frame
+
+	IEnumerator CustomerSummonCoroutine()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds (interval);
+
+			var CustomerName = Instantiate (Customer, transform.position, transform.rotation);
+			CustomerName.name = "Customer";
+		
+
+		}
+		
+	}
+
+}
---
+++ Assets/Data/Elf/Script/SummonItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SummonItem : MonoBehaviour 
+{
+
+	public GameObject TempObject;
+	public Vector3 Transformvec;
+	// Use this for initialization
+
+	public Vector3 itemSummonDestination;
+
+
+	public Vector3 ItemSummonDestination
+	{
+		get{return itemSummonDestination;}
+		set{ itemSummonDestination = value; }
+	}
+
+	void Start ()
+	{
+		Transformvec =	transform.localPosition = new Vector3 (transform.position.x,transform.position.y,transform.position.z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Instantiate (TempObject, Transformvec, transform.rotation);		
+	}
+}
