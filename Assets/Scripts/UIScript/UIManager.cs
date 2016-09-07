181a308a711fafa6d3c8ba39c13c1f3512a6d3a4
unknown <dds4120@naver.com>
2016-09-07 16:41:30 +0900

    2016.09.07 UI 상점 선택 만듬.

---
+++ Assets/Scripts/UIScript/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+	public Mode presentMode;
+
+	public GameObject storeCreate;
+	public StoreCreate storeCreateLogic;
+
+	public enum Mode
+	{
+		CreateStore,
+		SelectStore
+	};
+
+	public Mode PresentMode
+	{
+		get { return presentMode;}
+	}
+
+	public bool OnStoreCreate
+	{
+		get{return storeCreate.activeSelf; }
+	}
+
+	public void LinkElement()
+	{
+		storeCreate = GameObject.Find ("StoreCreate");
+
+		storeCreateLogic = storeCreate.GetComponent<StoreCreate> ();
+	}
+
+	public void ChangeUIMode(Mode UIMode)
+	{
+		switch (UIMode)
+		{
+			case Mode.CreateStore:
+				presentMode = Mode.CreateStore;
+				break;
+
+			case Mode.SelectStore:
+				presentMode = Mode.SelectStore;
+				break;		
+		}
+	}
+
+	public void InitializeModeCreateStore()
+	{
+				
+	}
+
+}
