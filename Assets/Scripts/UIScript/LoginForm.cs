9dae971050312d50427441a4aa32950cad0ab326
BaikSeongHyun <red96146@naver.com>
2016-09-29 01:32:42 +0900

    2016.09.29 로그인폼 기작 삽입

---
+++ Assets/Scripts/UIScript/LoginForm.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginForm : MonoBehaviour
+{
+	[SerializeField] InputField idInput;
+	[SerializeField] InputField passwordInput;
+
+	public string ID
+	{
+		get { return idInput.text; }
+	}
+
+	public string Password
+	{
+		get { return passwordInput.text; }	
+	}
+	
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (idInput.isFocused)
+		{
+			if (Input.GetKeyUp( KeyCode.Tab ))
+				passwordInput.Select();
+		}
+		
+		if (passwordInput.isFocused)
+		{
+			if (Input.GetKeyUp( KeyCode.KeypadEnter ))
+				;
+		}
+	}
+}
