d602612907f3ff611d476cbd02f92ff6ef0b3422
unknown <red96146@naver.com>
2016-09-29 19:52:57 +0900

    2016.09.29 기본 플로우 구현중

---
+++ Assets/Scripts/UIScript/StoreElement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreElement : MonoBehaviour
+{
+	[SerializeField] Image elementStoreImage;
+	[SerializeField] bool isDataLoad;
+	[SerializeField] bool isSelected;
+
+	void Awake()
+	{
+		LinkComponentElement();
+	}
+
+	public void LinkComponentElement()
+	{
+		elementStoreImage = GetComponentInChildren<Image>();
+	}
+
+	// update use storedata
+	public void UpdateStoreElement( Store data )
+	{
+
+	}
+
+	// clear component for null data
+	public void ClearComponent()
+	{
+
+	}
+	// on click method -> store select;
+	public void OnClickStoreSelect()
+	{
+	}
+}
